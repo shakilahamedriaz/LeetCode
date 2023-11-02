d58524e5838fefa85f0e6f3fc6607d6986622042
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-11-02 08:50:08 +0600

    d n f algo

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int best=INT_MIN;
+        int sum=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            sum=max(nums[i], sum + nums[i]);
+            best=max(best, sum);
+        }
+      return best;
+    }
+};
